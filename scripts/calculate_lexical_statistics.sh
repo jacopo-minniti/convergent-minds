313e0f6cf51844f85976ee3c5d5cb907192b5982
Jacopo Minniti <jacopo.minniti004@gmail.com>
2025-11-30 17:16:52 -0500

    small tweaks

--- scripts/calculate_lexical_statistics.sh
+++ scripts/calculate_lexical_statistics.sh
@@ -7,14 +7,13 @@
 #SBATCH --error=/scratch/jacopo04/convergent-minds/logs/calc_lexical-%j.err
 #SBATCH -D /scratch/jacopo04/convergent-minds
 
-module --force purge
-module load StdEnv/2023
-module load python/3.11.5
-module load cuda/12.6 cudnn
-module load gcc opencv arrow mpi4py
-
 export CUDA_LAUNCH_BLOCKING=1
 export PYTHONPATH="/scratch/jacopo04/convergent-minds/"
+
+module --force purge
+module load StdEnv/2023 python/3.11.5
+module load cuda/12.6 cudnn
+module load gcc opencv arrow mpi4py
 
 source .venv/bin/activate
 
