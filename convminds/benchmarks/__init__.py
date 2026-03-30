4eede3ef06e7d5d50a0fa6b5fd5f6f03f369828e
Jacopo Minniti <jacopo.minniti004@gmail.com>
2026-03-30 14:59:59 -0300

    huth dataset

--- convminds/benchmarks/__init__.py
+++ convminds/benchmarks/__init__.py
@@ -2,9 +2,11 @@ from convminds.interfaces import Benchmark
 
 from .base import InMemoryBenchmark
 from .pereira import PereiraBenchmark
+from .huth.benchmark import HuthBenchmark
 
 __all__ = [
     "Benchmark",
     "InMemoryBenchmark",
     "PereiraBenchmark",
+    "HuthBenchmark",
 ]
