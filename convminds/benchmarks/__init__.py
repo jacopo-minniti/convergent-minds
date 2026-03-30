9f5502b6b537e7eaec8c405f6ab7f4fd5591a2f5
Jacopo Minniti <jacopo.minniti004@gmail.com>
2026-03-30 12:55:46 -0300

    repo organization

--- convminds/benchmarks/__init__.py
+++ convminds/benchmarks/__init__.py
@@ -2,12 +2,9 @@ from convminds.interfaces import Benchmark
 
 from .base import InMemoryBenchmark
 from .pereira import PereiraBenchmark
-from .openneuro import HuthBenchmark, NarrativesBenchmark
 
 __all__ = [
     "Benchmark",
     "InMemoryBenchmark",
     "PereiraBenchmark",
-    "HuthBenchmark",
-    "NarrativesBenchmark",
 ]
