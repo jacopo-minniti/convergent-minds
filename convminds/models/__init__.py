48578a71bccb92f5996d3c0ed47708b13c3636b2
Jacopo Minniti <jacopo.minniti004@gmail.com>
2026-04-01 08:33:50 -0300

    huth+vae pipeline

--- convminds/models/__init__.py
+++ convminds/models/__init__.py
@@ -2,6 +2,5 @@ from .base import BrainLanguageModel
 from .mindllm import SpatialPrefixLM
 from .prompt_conditioned import PromptConditionedLM
 from .residual_steer import ResidualSteerLM
-from .steered_lm import DeepSteeredLM
 
-__all__ = ["BrainLanguageModel", "PromptConditionedLM", "DeepSteeredLM", "SpatialPrefixLM", "ResidualSteerLM"]
+__all__ = ["BrainLanguageModel", "PromptConditionedLM", "SpatialPrefixLM", "ResidualSteerLM"]
